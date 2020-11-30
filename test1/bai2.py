74f8b5149e546e8687b1547bcb73264c0f721532
500ping <ndt512x@gmail.com>
2020-11-30 23:32:38 +0700

    bai2

--- test1/bai2.py
+++ test1/bai2.py
@@ -1,27 +1,16 @@
 def bai2(x ,y):
-    if x > y:
-        return "x < y"
-
     count = 0
-
-    if x == y:
-        return count
-
-    while(True):
-        print(x, y)
-    
-        if x * 2 == y or x - 1 == y:
-            count += 1
-            break
+    while y > x:
+        if y % 2 == 0:
+            y /= 2
         else:
-            if x * 2 > (x - 1) * 2 and x * 2 > y and (x - 1) * 2 > y:
-                x -= 1
-            elif x < y:
-                x *= 2
-            else:
-                x -= 1
-            count += 1
-
+            y += 1
+        print(x, y)
+        count += 1
+    while y < x:
+        y += 1
+        print(x, y)
+        count += 1
     return count
 
 print(bai2(2, 5))
